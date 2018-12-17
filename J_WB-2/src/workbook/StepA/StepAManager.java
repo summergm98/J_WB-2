e7b9af1adee80fe5464dfb2235f57a861b48ccab
HuiKang Park <summergm98@naver.com>
2018-12-17 11:16:33 +0900

    #1 J_WB-2 생성 및 실행 확인

---
+++ J_WB-2/src/workbook/StepA/StepAManager.java
@@ -0,0 +1,30 @@
+package workbook.StepA;
+
+import java.util.Scanner;
+
+public class StepAManager {
+	public StepAManager() {
+		int menu;
+		Scanner s = new Scanner(System.in);
+		while(true) {
+			printf("---------------------\n");
+			printf("1. 나이계산\n");
+			printf("2. 온도변환\n");
+			printf("3. 직사각형 넓이계산\n");
+			printf("4. 아파트 평형 계산\n");
+			printf("5. 날짜계산\n");
+			printf("6. 점수계산\n");
+			printf("7. 파일 용량 계산\n");
+			printf("=>원하는 메뉴는?(0:exit) :");
+			menu = s.nextInt();
+			if(menu == 0) break;
+			switch(menu) {
+			case 1: 				
+				YearToAge yta = new YearToAge();
+				yta.printAge();
+			}
+		}
+		
+	}
+	void printf(String s){System.out.print(s);}
+}
